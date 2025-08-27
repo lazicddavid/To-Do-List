c085369625740657f71ace465c960719a14cbbef
David Lazic <lazic.ddavid@gmail.com>
2025-08-27 20:58:23 +0200

    added container

--- todolist.js
+++ todolist.js
@@ -1,3 +1,11 @@
-body {
-
-}
+//brisanje elemenata iz todo liste
+//treba ce se izbrisati prvi element iz niza
+//posle toga i sa ekrana
+//za brisanje sa ekrana, pogledaj metodu filter
+//kako ces da znas kad kliknes na to do, da ga povezes sa to do koji treba da izbrises iz niza
+//prilikom dodavanja to do u niz, napravi za svaki todo ID (pogledaj crypto.randomuuid
+//prilikom postavljanja to do na ekran, zakaci ID todo-a na list element
+//kad kliknem na to do, procitaj taj ID i to je ID todo-a koji treba da se izbrise
+/*
+const randomID = crypto.randomUUID();
+console.log(randomID);*/
